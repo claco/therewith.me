0521eb5d1bf1abd32df2ef6a80b431c3c5659383
Christopher H. Laco <claco@chrislaco.com>
2011-03-01 19:37:17 -0500

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TherewithMe::Application.config.secret_token = '0655553d6123dca324ff88c8161b76801e0b7970a4c03814e1c4b29d40a7782ea048486b59b402d31e9c241b78e062d4097e07355ba01923da8d2487e08c0a43'
